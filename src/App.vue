9bd2b01880f3c92394b40b78be76aabbe519f3f9
Paul Thebaud <paul.thebaud29@gmail.com>
2022-10-06 20:42:53 +0200

    feat: add header and footer

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   lang="ts"
   setup
 >
-import { ref } from 'vue';
-import { useTheme } from 'vuetify';
+import { computed, ref } from 'vue';
+import { useDisplay, useTheme } from 'vuetify';
 import PropsCard from './components/PropsCard.vue';
 import SignatureCard from './components/SignatureCard.vue';
 import fileToJsonObject from './utils/fileToJsonObject';
@@ -11,6 +11,7 @@ import { Signature } from './utils/signature';
 
 const STORAGE_KEY = 'signature.data';
 
+const { xs } = useDisplay();
 const theme = useTheme();
 
 let defaultData = {
@@ -43,6 +44,8 @@ const signatureData = ref(initialData);
 const jsonConfigFileInput = ref<HTMLInputElement | null>(null);
 const jsonConfigFileError = ref<Error | undefined>(undefined);
 
+const size = computed(() => (xs.value ? 'small' : 'large'));
+
 const onSignatureDataUpdate = (data: Signature) => {
   signatureData.value = data;
 
@@ -68,10 +71,37 @@ const onJsonConfigFileChange = async (event: Event) => {
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <h1 class="text-uppercase text-h3 text-sm-h2 text-center my-8">
-          Mail signature
-        </h1>
+      <v-container class="my-4 my-md-12">
+        <header class="mb-4 mb-md-8">
+          <h1 class="text-uppercase text-h4 text-sm-h2 text-center mb-4">
+            <span class="text-uppercase">
+              Mail signature
+            </span>
+          </h1>
+          <p class="text-body-1 text-sm-h5 text-center mb-4">
+            Open source mail signature generator with embedded base64 images
+            and compatible with all mailboxes.
+          </p>
+          <div class="d-flex align-center justify-center flex-wrap">
+            <v-btn
+              :size="size"
+              href="url"
+              target="_blank"
+              rel="noopener"
+              variant="text"
+            >
+              <v-icon
+                icon="mdi-github"
+                size="24"
+                start
+              />
+              GitHub
+              <span class="d-sr-only">
+                (open in new tab)
+              </span>
+            </v-btn>
+          </div>
+        </header>
         <v-row>
           <v-col
             cols="12"
@@ -125,6 +155,21 @@ const onJsonConfigFileChange = async (event: Event) => {
             />
           </v-col>
         </v-row>
+        <footer class="mt-4 mt-md-8">
+          <p class="text-body-1 text-center mb-0">
+            Made by
+            <a
+              href="https://github.com/paul-thebaud"
+              rel="noopener"
+              target="_blank"
+            >
+              Paul Thébaud
+              <span class="d-sr-only">
+                (open in new tab)
+              </span>
+            </a>
+          </p>
+        </footer>
       </v-container>
     </v-main>
   </v-app>
